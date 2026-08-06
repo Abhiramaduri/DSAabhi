23cef53d2a85dfe3ab787ced98d011342e03ea76
Aduri Venakat Abhiram <aduriabhiram1234@gmail.com>
2026-08-06 21:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (85.55%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for i in range(len(arr)):
+            if arr[i] % 2 == 1:
+                count +=1
+                if count ==3:
+                    return True
+            else: 
+                count =0 
+        return False
+
+
+
