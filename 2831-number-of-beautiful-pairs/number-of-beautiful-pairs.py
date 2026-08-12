f13615fa93c096c864b61b01cd6917ce249cad75
Aduri Venakat Abhiram <aduriabhiram1234@gmail.com>
2026-08-12 09:15:28 +0530

    Time: 242 ms (39.51%) | Memory: 12.4 MB (60.49%) - LeetSync

--- 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
@@ -11,15 +11,11 @@ class Solution(object):
 
         ans = 0
         n = len(nums)
-        
-        # Check every pair (i, j) where i < j
         for i in xrange(n):
             first_digit = int(str(nums[i])[0])
             
             for j in xrange(i + 1, n):
                 last_digit = nums[j] % 10
-                
-                # If they are coprime, increment our pair counter
                 if gcd(first_digit, last_digit) == 1:
                     ans += 1
                     
