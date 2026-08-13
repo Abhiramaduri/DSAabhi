957f7f7db34442914407bf1123bf3d2f24dbea9f
Aduri Venakat Abhiram <aduriabhiram1234@gmail.com>
2026-08-13 22:10:50 +0530

    Time: 318 ms (96.72%) | Memory: 41.8 MB (5.33%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
@@ -1,19 +1,25 @@
 class Solution:
-    def findPrimePairs(self, n: int) -> List[List[int]]:
+    def findPrimePairs(self, n: int) -> list[list[int]]:
         if n <= 3:
+            return []
+        if n % 2 != 0:
+            target = n - 2
+            if target >= 2:
+                for i in range(2, int(target**0.5) + 1):
+                    if target % i == 0:
+                        return []
+                return [[2, target]]
             return []
         is_prime = [True] * (n + 1)
         is_prime[0] = is_prime[1] = False
         
         for i in range(2, int(n**0.5) + 1):
             if is_prime[i]:
-                for j in range(i * i, n + 1, i):
-                    is_prime[j] = False                    
+                is_prime[i*i : n+1 : i] = [False] * len(is_prime[i*i : n+1 : i])
+                    
         ans = []
         for x in range(2, (n // 2) + 1):
-            y = n - x
-            if is_prime[x] and is_prime[y]:
-                ans.append([x, y])
+            if is_prime[x] and is_prime[n - x]:
+                ans.append([x, n - x])
                 
         return ans
-        
