50c16ce285a84e36ec8a5c826b2f49913f0b9a57
Aduri Venakat Abhiram <aduriabhiram1234@gmail.com>
2026-08-13 22:07:05 +0530

    Time: 208 ms (37.46%) | Memory: 19.2 MB (90.56%) - LeetSync

--- 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.py
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.py
@@ -6,10 +6,8 @@ class Solution:
         for i in range(n):
             running_lcm = nums[i]
             for j in range(i, n):
-                # Update running LCM
                 running_lcm = (running_lcm * nums[j]) // math.gcd(running_lcm, nums[j])
                 
-                # Optimization: break if running LCM cannot form K
                 if k % running_lcm != 0:
                     break
                     
