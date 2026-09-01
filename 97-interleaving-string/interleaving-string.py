917fa9f813d013113b47849a5c7b2b0a13150670
Aduri Venakat Abhiram <aduriabhiram1234@gmail.com>
2026-09-01 23:29:03 +0530

    Time: 52 ms (53.25%) | Memory: 20.5 MB (16.20%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,21 +1,25 @@
+from collections import deque
+
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         m, n = len(s1), len(s2)
+        
         if m + n != len(s3):
             return False
-        if m < n:
-            s1, s2 = s2, s1
-            m, n = n, m
-        dp = [False] * (n + 1)
-        dp[0] = True
-        for j in range(1, n + 1):
-            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
-        for i in range(1, m + 1):
-            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
-            
-            for j in range(1, n + 1):
-                dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
-                
-        return dp[n]
+        queue = deque([(0, 0)])
 
+        visited = {(0, 0)}
         
+        while queue:
+            i, j = queue.popleft()
+
+            if i == m and j == n:
+                return True
+            if i < m and s1[i] == s3[i + j] and (i + 1, j) not in visited:
+                visited.add((i + 1, j))
+                queue.append((i + 1, j))
+            if j < n and s2[j] == s3[i + j] and (i, j + 1) not in visited:
+                visited.add((i, j + 1))
+                queue.append((i, j + 1))
+                
+        return False
